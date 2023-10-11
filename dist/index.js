1637cad4066f48cf1b4fc2f655ed52ea00ad4dd8
Collins Kariuki <collinsmunkary@gmail.com>
2023-10-11 16:35:07 +0300

    Refactoring the code using interface to achieve code reuse

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_js_1 = require("./MatchReader.js");
 const MatchResult_js_1 = require("./MatchResult.js");
-const reader = new MatchReader_js_1.MatchReader('football.csv');
-reader.read();
+const CsvFileReader_js_1 = require("./CsvFileReader.js");
+// Create an object that satisfies the 'DataReader' Interface
+const csvFileReader = new CsvFileReader_js_1.CsvFileReader('football.csv');
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader_js_1.MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult_js_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
